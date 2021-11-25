2a97a5425d1f36ab3bb96999d6979915258656ea
PogChamp <a1670764@student.adelaide.edu.au>
2021-11-26 00:15:27 +1100

    revamping dataframe

--- util.cpp
+++ util.cpp
@@ -12,11 +12,26 @@ namespace util {
 			}
 		}
 		else {
-			std::cout << "{";
 			for (int i = 0; i < vec.size() - 1; i++) {
 				std::cout << vec[i] << ", ";
 			}
-			std::cout << vec.back() << "}" << std::endl;
+			std::cout << vec.back() << std::endl;
 		}
+	}
+
+	std::vector<std::string> split(std::string str, char delimiter = ',') {
+		std::vector<std::string> output;
+		// make substrs based on delimiter
+		int start = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] == delimiter) {
+				output.push_back(str.substr(start, i - start));
+				start = i + 1;
+			}
+			else if (i == str.length() - 1) {
+				output.push_back(str.substr(start, i - start + 1));
+			}
+		}
+		return output;
 	}
 }
